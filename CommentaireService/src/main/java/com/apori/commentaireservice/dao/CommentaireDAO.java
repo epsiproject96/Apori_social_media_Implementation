97e715ed02baf2243683ab5d5eefc9f3177998b1
Didier Malembe <malembedidier24@gmail.com>
2020-12-14 17:25:39 +0100

    Maj du microservice - CommentaireService

---
+++ CommentaireService/src/main/java/com/apori/commentaireservice/dao/CommentaireDAO.java
@@ -0,0 +1,12 @@
+package com.apori.commentaireservice.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.apori.commentaireservice.model.Commentaire;
+
+import org.springframework.data.repository.CrudRepository;
+
+@Repository
+public interface CommentaireDAO extends CrudRepository<Commentaire,Integer> {
+
+}
