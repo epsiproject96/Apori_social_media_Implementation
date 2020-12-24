37b5037ee74936bdab24933f126eff52d44fadf0
Didier Malembe <malembedidier24@gmail.com>
2020-12-24 20:01:45 +0100

    Ajout du Microservice - DiscoveryService

---
+++ DiscoveryService/src/test/java/com/apori/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.apori.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
