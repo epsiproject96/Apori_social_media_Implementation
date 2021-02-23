10ddc04e1816ca36020f57c422660f1549633f46
ibench <61089562+ibench@users.noreply.github.com>
2021-02-23 09:20:32 +0100

    last commit

---
+++ MessagerieService/src/main/java/com/apori/messagerieservice/controller/ChatController.java
@@ -0,0 +1,25 @@
+package com.apori.messagerieservice.controller;
+
+import com.apori.messagerieservice.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+
+@MessageMapping("/chat.register")
+@SendTo("/topic/public")
+public class ChatController {
+    public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor){
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.send")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage){
+        return chatMessage;
+    }
+}
